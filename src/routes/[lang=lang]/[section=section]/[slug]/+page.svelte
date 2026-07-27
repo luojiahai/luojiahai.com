5261260a1f2c502bfd5ab75a62f016adc08b0c80
luojiahai <ljiahai@hotmail.com>
2026-07-27 13:41:15 +1000

    Increase primary title scale

--- src/routes/[lang=lang]/[section=section]/[slug]/+page.svelte
+++ src/routes/[lang=lang]/[section=section]/[slug]/+page.svelte
@@ -74,7 +74,7 @@
       {/each}
     </div>
     <h1
-      class="max-w-[28ch] font-serif text-2xl font-bold leading-[1.08] tracking-[-0.025em] text-printer-ink dark:text-printer-ink-dark"
+      class="max-w-[28ch] font-serif text-[2rem] font-bold leading-[1.08] tracking-[-0.025em] text-printer-ink dark:text-printer-ink-dark"
     >
       {post.title}
     </h1>
